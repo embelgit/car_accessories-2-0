53494b1e15bf78f3535d78cd73cb6bfa3019a502
Umesh <Umesh@Umesh-PC>
2020-07-17 20:23:21 +0530

    supplier payment by priya

--- SMT/src/com/smt/helper/SupplierAccountDetailsHelper.java
+++ SMT/src/com/smt/helper/SupplierAccountDetailsHelper.java
@@ -127,7 +127,7 @@ public class SupplierAccountDetailsHelper {
 					
 				
 				//Double newBal1 = Double.valueOf(newBal);
-					System.out.println(newBal+"NEW BALANCE");
+					System.out.println(newBal+"NEWly BALANCE");
 					
 						bean.setBalance(newBal);
 					
